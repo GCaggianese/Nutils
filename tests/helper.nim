5f2a84afe209de1c7b3a83f2941736f66ad4d065
Germán Caggianese <german.caggianese@pm.me>
2025-07-07 00:11:00 -0300

    chore: small refactor

--- tests/helper.nim
+++ tests/helper.nim
@@ -1,12 +1,6 @@
 import std/strutils
 import std/os
 
-# proc openFilesToTest*(file: string): (File, File) =
-#   let f_e = open("./expected_frontpatch/expected_"&file&".md", fmRead)
-#   let file_obtained = "./obtained_frontpatch/obtained_"&file&".md"
-#   let f_o = open(file_obtained, fmRead)
-#   return (f_e, f_o)
-
 proc openFilesToTest*(name: string): (File, File) =
   # Always resolve relative to this helper’s location in `tests` folder
   let testsDir = currentSourcePath().parentDir
